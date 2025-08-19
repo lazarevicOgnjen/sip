78fc4231d6d860fc3e84538fc0b0973de1fd06b6
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-08-19 15:42:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import time
 
 # Chrome options
 chrome_options = Options()
